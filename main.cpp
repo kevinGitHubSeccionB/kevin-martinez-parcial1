95d65796e8992b0e6a37b7c20cd66526d106e3be
Kevin-Martinez <kmartinezc22@miumg.edu.gt>
2024-03-02 10:52:16 -0600

    ejercicio 2.1 realisado por kevin ivan martinez cartagena 0909-23-16182

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 1,b = 2;
+    cout << "Viembenido la sumador de numeros impares"<<endl;
+    while (a <=50){
+    if (a + 2){
+    }
+    a++;
+    }
+
+    while (b <=50){
+        if (b + 2){
+    }
+    b++;
+    }
+
+    return 0;
+}
